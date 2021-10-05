41d8a4feafd603b7fb8eae4c1b2f800ff63837de
Lincoln Roebrts <lojorob9@gmail.com>
2021-10-04 19:32:01 -0600

    commiting

---
+++ app.js
@@ -0,0 +1,5 @@
+let start = $(".navItem").css("color", "white")
+let second = $("#nav").css("background-color", "#145")
+let third = $("li:even").css("background-color", "#ccc")
+let fourth = $("li:odd").css("background-color", "#aaa")
+let fifth = $("#myInput").val("Lincoln Roberts")
